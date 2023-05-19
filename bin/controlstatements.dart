b5f080972d7a7a022ecae73fcdd176a29b8e50b3
rahul <rahulrahul19042002@gmail.com>
2023-05-19 12:19:50 +0530

    commit

--- bin/controlstatements.dart
+++ bin/controlstatements.dart
@@ -1,19 +1,22 @@
 
-
-
-
-void main()
-{
-
+void main() {
   print("Hi user");
-  String username="rahul";
-  String password="rahul123";
-  if(username=="rahul" && password=="rahul123")
-  {
+  String username = "rahul";
+  String password = "rahul123";
+  int OTP = 2345;
+  if (username == "rahul" && password == "rahul123") {
     print("login successful");
+    if (OTP == 2345) {
+      print("OTP verifiyed login successfully");
+    }
+    else
+      {
+        print("OIP verification failed");
+      }
+    }
+    else {
+      print("Not successful");
+    }
+    print("Thank you");
   }
-  else{
-    print("Not successful");
-  }
-  print("Thank you");
-}
+

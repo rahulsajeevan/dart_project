54316c1a6036ccbccd28a3590b1d59b236f0c10a
rahul <rahulrahul19042002@gmail.com>
2023-05-23 12:02:15 +0530

    commit

--- bin/controlstatements.dart
+++ bin/controlstatements.dart
@@ -1,23 +1,33 @@
 
 void main()
 {
-  int size=7;
-  if(size==5)
-    {
-      print("shoe size is 5");
-    }
-  else if(size==6)
-  {
-    print("shoe size is 6");
-  }
-  else if(size==7)
-    {
-      print("shoe size is 7");
-    }
-  else
-    {
-      print("shoe size is not available");
-    }
+  String shirtsize="l";
+  switch(shirtsize)
+      {
+    case "s":
 
-  }
+        print(" size is small");
+        break;
+
+    case "m":
+      print(" size is medium");
+      break;
+
+    case "l":
+      print(" size is large");
+      break;
+
+    case "xl":
+      print(" size is extra large");
+      break;
+
+    case "xxl":
+      print(" size is extra extra large");
+      break;
+
+    default:
+      print("size is not available");
+
+      }
+}
 

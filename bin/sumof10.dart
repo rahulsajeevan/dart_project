fb6f1662c445d6e46d86d56d0d56894637b269b0
rahul <rahulrahul19042002@gmail.com>
2023-06-20 13:12:10 +0530

    commit

--- bin/sumof10.dart
+++ bin/sumof10.dart
@@ -1,11 +1,16 @@
 void main()
 {
   int i;
-  for(i=1;i<=10;i++)
-    {
-      if(i%2==0)
-        {
-          print(i);
-        }
+  int sum1=0;
+  int sum2=0;
+  for(i=1;i<=20;i++) {
+    if (i % 2 == 0) {
+      sum1+=i;
     }
+      else {
+        sum2+=i;
+      }
+  }
+  print("even sum=$sum1");
+  print("odd sum=$sum2");
 }

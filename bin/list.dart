246b93b2b3123bef5c58f28663754a8f500d5f4b
rahul <rahulrahul19042002@gmail.com>
2023-06-20 15:22:01 +0530

    commit

---
+++ bin/list.dart
@@ -0,0 +1,11 @@
+void main()
+{
+  List l1=[];
+  var l2=[];
+  List l3=[1,2,'hello','hi',5.6];
+  List <int> l4=[1,2,3,4,5];
+  print("l1=$l1");
+  print("l2=$l2");
+  print("l3=$l3");
+  print("l4=$l4");
+}
